a98deba2b0f328704f500ccf2d329e1451710d15
vanal <vanalekoncev@gmail.com>
2024-04-03 20:45:25 +0500

    Реализованные классы. Леконцев

---
+++ Documents_Lekontsev/Classes/DocumentContext.cs
@@ -0,0 +1,101 @@
+﻿using Documents_Lekontsev.Interfaces;
+using Documents_Lekontsev.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents_Lekontsev.Classes
+{
+    public class DocumentContext : Document, IDocument
+    {
+
+        public OleDbDataReader GetDataDocuments()
+        {
+            OleDbConnection connection = Common.DBConnect.Connection();
+            OleDbDataReader dataDocuments = Common.DBConnect.Query("SELECT * FROM [Документы]", connection);
+            return dataDocuments;
+        }
+
+        public List<DocumentContext> AllDocuments(OleDbDataReader oleDbDataReader)
+        {
+            List<DocumentContext> allDocuments = new List<DocumentContext>();
+
+            OleDbConnection connection = Common.DBConnect.Connection();
+            OleDbDataReader dataDocuments = Common.DBConnect.Query("SELECT * FROM [Документы]", connection);
+            while (dataDocuments.Read())
+            {
+                DocumentContext newDocument = new DocumentContext();
+
+                newDocument.id = dataDocuments.GetInt32(0);
+                newDocument.src = dataDocuments.GetString(1);
+                newDocument.name = dataDocuments.GetString(2);
+                newDocument.user = dataDocuments.GetString(3);
+                newDocument.id_document = dataDocuments.GetString(4);
+                newDocument.date = dataDocuments.GetDateTime(5);
+                newDocument.status = dataDocuments.GetInt32(6);
+                newDocument.vector = dataDocuments.GetString(7);
+
+                allDocuments.Add(newDocument);
+            }
+
+            Common.DBConnect.CloseConnection(connection);
+            return allDocuments;
+
+        }
+        public void Save(bool Update = false)
+        {
+            if (Update)
+            {
+                OleDbConnection connection = Common.DBConnect.Connection();
+                Common.DBConnect.Query("UPDATE [Документы] " +
+                                          "SET " +
+                                                $"[Изображение] = '{this.src}', " +
+                                                $"[Наименование] = '{this.name}', " +
+                                                $"[Ответственный] = '{this.user}', " +
+                                                $"[Код документа] = '{this.id_document}', " +
+                                                $"[Дата поступления] = '{this.date.ToString("dd.MM.yyyy")}', " +
+                                                $"[Статус] = {this.status}, " +
+                                                $"[Направление] = '{this.vector}' " +
+                                            $"WHERE [Код] = {this.id}", connection);
+
+                Common.DBConnect.CloseConnection(connection);
+            }
+            else
+            {
+                OleDbConnection connection = Common.DBConnect.Connection();
+                Common.DBConnect.Query("INSERT INTO " +
+                                                "[Документы] " +
+                                                      "([Изображение], " +
+                                                      "[Наименование], " +
+                                                      "[Ответственный], " +
+                                                      "[Код документа], " +
+                                                      "[Дата поступления], " +
+                                                      "[Статус], " +
+                                                      "[Направление]) " +
+                                        "VALUES (" +
+                                            $"'{this.src}', " +
+                                            $"'{this.name}', " +
+                                            $"'{this.user}', " +
+                                            $"'{this.id_document}', " +
+                                            $"'{this.date.ToString("dd.MM.yyyy")}', " +
+                                            $"{this.status}, " +
+                                            $"'{this.vector}')", connection);
+
+                Common.DBConnect.CloseConnection(connection);
+            }
+        }
+
+        public void Delete()
+        {
+            OleDbConnection connection = Common.DBConnect.Connection();
+            Common.DBConnect.Query($"DELETE FROM [Документы] WHERE [Код] = {this.id}", connection);
+            Common.DBConnect.CloseConnection(connection);
+        }
+
+    }
+}
+
+   
