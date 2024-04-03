929552f7b372166dc86f3a698ee4787198ee5f15
vanal <vanalekoncev@gmail.com>
2024-04-03 20:22:44 +0500

    Реализованные интерфейсы. Леконцев

---
+++ Documents_Lekontsev/Interfaces/IDocument.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents_Lekontsev.Interfaces
+{
+    public interface IDocument
+    {
+        void Save(bool Update = false);
+
+        List<Documents_Lekontsev.Classes.DocumentContext> AllDocuments(System.Data.OleDb.OleDbDataReader oleDbDataReader);
+
+        void Delete();
+    }
+}
